94ec6328b7b970641459ad36c95378ef10af614c
Chester2171 <Chester2171@gmail.com>
2020-08-24 16:10:00 -0700

    KilometerToMiles

---
+++ KilometersToMiles/Conversion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Dynamic;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConvertingDistance
+{
+    public class Conversion
+    {
+        public static double KilometersToMiles(double input)
+        {
+            input = input / 1.609;
+            return Math.Round(input, 2);
+        }
+        public static double MilesToKilometers(double input)
+        {
+            input = input * 1.609;
+            return Math.Round(input, 2);
+        }
+    }
+}
